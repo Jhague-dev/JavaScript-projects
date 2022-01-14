de500ff93c34d9f5d8fc325521c87eb54340b43c
Jhague87 <96696987+Jhague-dev@users.noreply.github.com>
2022-01-14 18:32:59 -0500

    Update Main.js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/Main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/Main.js
@@ -1,8 +1,8 @@
 window.alert("Hello, World!"); //This string will display "Hello, world"//
-var A = "What is a \"kumquat\""; //This assigns variable A to the string shown//
-document.write("Hello, World?" + A); //this displays "hello, world" concatenated with var A//
+var A = "Hello world"; //This assigns variable A to the string shown//
 var Characters = "roster", Grappler = "Bulk-o", Zoner = "keepaway", Shoto = "ripoff", puppet = "marionette";
 document.write(Grappler);//this will display the name of the assigned variable from those listed above//
 document.write (4 + 5);//this will display the result of the statement shown in parenthesis//
-var B = "this";
+var B = "What is a \"kumquat\"";//newly assigned var b//
 document.write(B.fontcolor('blue'));//albeit deprecated, this prints var b, and paints it blue//
+document.write(A+B);//concatenated var a + b//
