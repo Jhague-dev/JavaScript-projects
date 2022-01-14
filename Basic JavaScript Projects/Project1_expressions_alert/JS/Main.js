7df259f87d8241705d256cb89c5d481f05204d32
Jhague87 <96696987+Jhague-dev@users.noreply.github.com>
2022-01-14 17:26:32 -0500

    first assignment submission

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/Main.js
@@ -0,0 +1,6 @@
+window.alert("Hello, World!"); //This string will display "Hello, world"//
+var A = "What is a \"kumquat\""; //This assigns variable A to the string shown//
+document.write("Hello, World?" + A); //this displays "hello, world" concatenated with var A//
+var Characters = "roster", Grappler = "Bulk-o", Zoner = "keepaway", Shoto = "ripoff", puppet = "marionette";
+document.write(Grappler);//this will display the name of the assigned variable from those listed above//
+document.write (4 + 5);//this will display the result of the statement shown in parenthesis//
