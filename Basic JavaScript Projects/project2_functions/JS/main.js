79f8302c688a0370eb1a3f928b0cf73082f5f9dd
Jhague87 <96696987+Jhague-dev@users.noreply.github.com>
2022-01-17 14:14:17 -0500

    project upload

---
+++ Basic JavaScript Projects/project2_functions/JS/main.js
@@ -0,0 +1,18 @@
+function My_First_Function() {
+    var str = "first";
+    document.getElementById("Button_Text").innerHTML = str;
+//this first function sets up the first button on the page//
+}
+
+function My_Second_Function() {
+    var str = "second";
+    document.getElementById("Button_Text").innerHTML = str;
+    //this does the same as the above function, just for a second button//
+}
+
+function myFunction() {
+    var sentence ="This sentence is";
+    sentence += "being concatenated";
+    document.getElementById("concatenate").innerHTML = sentence;
+    //this functions sole purpose is to demonstrate a concatenated string using the += operator.//
+}
